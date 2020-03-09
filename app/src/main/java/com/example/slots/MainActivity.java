c8a1dd8d3a9bd75e8993959655be221af85489d6
Dakota Nickel <dakota@Dakotas-iMac.local>
2020-03-09 18:30:27 -0400

    third commit

--- app/src/main/java/com/example/slots/MainActivity.java
+++ app/src/main/java/com/example/slots/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -16,10 +17,18 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageView slotOne, slotTwo, slotThree;
     private Drawable cherryImage, pearImage, strawberryImage, grapeImage;
+    private Drawable[] images;
     private TextView pointsView;
     private SeekBar speedBar;
-    private Button startButton, rulesButton;
+    private Button startButton;
     private int points;
+    private Handler handler;
+    private SlotImageChangerOne imageChangerOne;
+    private int nextImageOne = 1;
+    private SlotImageChangerTwo imageChangerTwo;
+    private int nextImageTwo = 2;
+    private SlotImageChangerThree imageChangerThree;
+    private int nextImageThree = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,31 +45,80 @@ public class MainActivity extends AppCompatActivity {
         pearImage = getDrawable(R.drawable.pear);
         strawberryImage = getDrawable(R.drawable.strawberry);
         grapeImage = getDrawable(R.drawable.grape);
+        images = new Drawable[]{cherryImage, pearImage, strawberryImage, grapeImage};
         pointsView = findViewById(R.id.pointsView);
         speedBar = findViewById(R.id.speedSeekBar);
         startButton = findViewById(R.id.startStopButton);
-        rulesButton = findViewById(R.id.rulesButton);
-        points = 0;
+        points = 60;
+        handler = new Handler();
+        imageChangerOne = new SlotImageChangerOne();
+        imageChangerTwo = new SlotImageChangerTwo();
+        imageChangerThree = new SlotImageChangerThree();
     }
 
     public void startSlots(View view) {
+        if(startButton.getText().toString().equals("Start")) {
+            handler.postDelayed(imageChangerOne, 0);
+            handler.postDelayed(imageChangerTwo, 0);
+            handler.postDelayed(imageChangerThree, 0);
+            startButton.setText("Stop");
+        } else {
+            handler.removeCallbacks(imageChangerOne);
+            handler.removeCallbacks(imageChangerTwo);
+            handler.removeCallbacks(imageChangerThree);
+            startButton.setText("Start");
+        }
         /**
-         * Todo:
-         * 1. Make rules activity
-         * 2. Get images to randomize when startButton is pressed and to stop when stop button is pressed
+         * Todo
+         * 1. Get images to randomize when startButton is pressed and to stop when stop button is pressed
+         * 2. if (nextImageOne == 2) this means slowOne has a cherry and therefore +10 points
+         * 3. Play with speed
          */
-//        slotOne.setImageDrawable(grapeImage);
-//        slotTwo.setImageDrawable(pearImage);
-//        slotThree.setImageDrawable(strawberryImage);
 //        pointsView.setText("Points: 60");
 //        int seekBarValue = speedBar.getProgress();
-//        startButton.setText("Stop");
-//        Log.w("fatal", seekBarValue+"");
+
+//        handler.postDelayed(imageChangerOne, 0);
+//        handler.postDelayed(imageChangerTwo, 0);
+//        handler.postDelayed(imageChangerThree, 0);
     }
 
     public void rulesClicked(View view) {
         Intent intent = new Intent(this, RulesActivity.class);
         intent.putExtra("POINTS", points);
         startActivity(intent);
+    }
+
+    private class SlotImageChangerOne implements Runnable {
+        @Override
+        public void run() {
+            if(handler.hasCallbacks(imageChangerOne)) {
+                Drawable imageToDraw = images[nextImageOne];
+                nextImageOne = (nextImageOne == 3) ? 0 : nextImageOne+1;
+                slotOne.setImageDrawable(imageToDraw);
+                handler.postDelayed(imageChangerOne, 500);
+            }
+        }
+    }
+    private class SlotImageChangerTwo implements Runnable {
+        @Override
+        public void run() {
+            if(handler.hasCallbacks(imageChangerTwo)) {
+                Drawable imageToDraw = images[nextImageTwo];
+                nextImageTwo = (nextImageTwo == 3) ? 0 : nextImageTwo+1;
+                slotTwo.setImageDrawable(imageToDraw);
+                handler.postDelayed(imageChangerTwo, 250);
+            }
+        }
+    }
+    private class SlotImageChangerThree implements Runnable {
+        @Override
+        public void run() {
+            if(handler.hasCallbacks(imageChangerThree)) {
+                Drawable imageToDraw = images[nextImageThree];
+                nextImageThree = (nextImageThree == 3) ? 0 : nextImageThree+1;
+                slotThree.setImageDrawable(imageToDraw);
+                handler.postDelayed(imageChangerThree, 400);
+            }
+        }
     }
 }
